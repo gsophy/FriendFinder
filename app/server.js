8c3856f64378004cf380c2f3d408663d77c2f48a
gsophy <georgesophy@gmail.com>
2018-08-17 10:59:34 -0400

    Server.js file updated

---
+++ app/server.js
@@ -0,0 +1,21 @@
+//First we need to define the dependencies
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require ("path");
+
+var app = express();
+var port = process.env.PORT || 3000;
+
+//If I wanted to serve up static content, I would include the following command;
+//app.use(express.static(__dirname + "/app/pblic/css")) app/public/css is the path to the CSS Folder
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true}));
+
+//Define the routes for HTML and API
+require("./app/routing/apiRoutes.js") (app);
+require("./app/routing/htmlRoutes.js") (app);
+
+app.listen(PORT, function {
+    console.log("Server is running on Port " + PORT);
+});
